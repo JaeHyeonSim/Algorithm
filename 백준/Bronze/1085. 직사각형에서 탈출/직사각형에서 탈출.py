88e539012a6c3379af8778f538ee331cbff304d0
Sim JaeHyeon <112995965+JaeHyeonSim@users.noreply.github.com>
2024-01-11 23:37:15 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,11 +1,2 @@
 x, y, w, h = map(int, input().split())
-answer = x
-
-if answer > w - x:
-    answer = w - x
-if answer > y:
-    answer = y
-if answer > h - y:
-    answer = h - y
-
-print(answer)
+print(min(x, w - x, y, h - y))
