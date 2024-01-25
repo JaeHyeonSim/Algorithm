869acf6b9dcac19232ef76863123792a80b884e0
Sim JaeHyeon <112995965+JaeHyeonSim@users.noreply.github.com>
2024-01-26 00:21:56 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 3300 ms, Memory: 39368 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,22 @@
+import sys
+
+prime = [1 for i in range(1000000)]
+for i in range(2, 1000000):
+    if i * i >= 1000000:
+        break
+    for j in range(i * i, 1000000, i):
+        prime[j] = 0
+
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0:
+        break
+    
+    find = 0
+    for i in range(3, n - 2, 2):
+        if prime[i] == 1 and prime[n - i] == 1:
+            print("{0} = {1} + {2}".format(n, i, n - i))
+            find = 1
+            break
+    if find == 0:
+        print("Goldbach's conjecture is wrong.")
